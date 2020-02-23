f3680344ad9c1421d1253fa9ff151779bbf51868
Cooper Barth <cooperfbarth@gmail.com>
2020-02-22 18:54:00 -0600

    added more paths

--- paths.js
+++ paths.js
@@ -1,24 +1,24 @@
 var paths = [
     // Path 1
     [{
-        latitude: 41.925589,
-        longitude: -87.646286
+        latitude: 41.924903,
+        longitude: -87.643486
     },
     {
-        latitude: 41.925589,
-        longitude: -87.646265
+        latitude: 41.924184,
+        longitude: -87.643465
     },
     {
-        latitude: 41.925589,
-        longitude: -87.646265
+        latitude: 41.923674,
+        longitude: -87.643465
     },
     {
-        latitude: 41.925589,
-        longitude: -87.646243
+        latitude: 41.923179,
+        longitude: -87.643443
     },
     {
-        latitude: 41.925589,
-        longitude: -87.646179
+        latitude: 41.921662,
+        longitude: -87.643479
     }],
 
     // Path 2
@@ -41,5 +41,27 @@ var paths = [
     {
         latitude: 41.921662,
         longitude: -87.646179
+    }],
+
+    // Path 3
+    [{
+        latitude: 41.924903,
+        longitude: -87.640486
+    },
+    {
+        latitude: 41.924184,
+        longitude: -87.640465
+    },
+    {
+        latitude: 41.923674,
+        longitude: -87.640465
+    },
+    {
+        latitude: 41.923179,
+        longitude: -87.640443
+    },
+    {
+        latitude: 41.921662,
+        longitude: -87.640479
     }]
 ];
