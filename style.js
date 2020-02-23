2d554b5df2ffa379fe8eeec7e33405bf759fe781
Cooper Barth <cooperfbarth@gmail.com>
2020-02-22 18:39:16 -0600

    cleaned up code a bit

---
+++ style.js
@@ -0,0 +1,13 @@
+var markerColors = [
+    'blue',
+    'gold',
+    'red',
+    'green',
+    'orange',
+    'yellow',
+    'violet',
+    'grey',
+    'black'
+].map(color => `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-${color}.png`);
+
+var mapStyle = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
